ae7355496b0694fe755b1d4c670b092d1fcf78d2
forgedsoft <75130477+forgedsoft@users.noreply.github.com>
2021-06-07 18:50:26 +0300

    Update https-update.php

--- https-update.php
+++ https-update.php
@@ -19,7 +19,7 @@
 // This code is provided 'as is' without any guarantee. Use it in your own risk. Remember to backup Before running this script.
 // How it works:
 // This php script loads database connection data from wp-config.php file on the website root.
-// After database connection, the script will run a wutery to get 'siteurl' from wp_options table. 
+// After database connection, the script will run a query to get 'siteurl' from wp_options table. 
 // 4 links are created that may have been set on posts url's. With www, without www, with www and https, witout www and https.
 // All these cases are updated to current siteurl with https.
 // This script will also correct urls and permalinks that have or not www.
